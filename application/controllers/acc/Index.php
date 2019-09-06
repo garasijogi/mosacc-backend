8dab2953765e2219da8d936878687d7aecd5c8c6
Ryumada Rizuki <rizki3366@gmail.com>
2019-09-06 09:31:30 +0700

    Initial Commit

---
+++ application/controllers/acc/Index.php
@@ -0,0 +1,15 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends CI_Controller {
+    
+    public function index()
+    {
+        echo "Hello World";
+    }
+    
+}
+
+/* End of file  index.php */
+
