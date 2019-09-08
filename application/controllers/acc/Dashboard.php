cea045c3e18e1237c7e5d48bc3938299eb0150c5
Ryumada Rizuki <rizki3366@gmail.com>
2019-09-08 07:56:04 +0700

    Initial Commit #2

--- application/controllers/acc/Dashboard.php
+++ application/controllers/acc/Dashboard.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class Dashboard extends CI_Controller {
     
     public function index()
     {
