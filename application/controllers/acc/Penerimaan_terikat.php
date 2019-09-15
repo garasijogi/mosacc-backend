300cb1ad471e6fae170fd9c271babf4c9517b1b2
Algorithm <49584601+algorithmsevolve@users.noreply.github.com>
2019-09-15 15:38:00 +0700

    update 3
    penyesuaian

--- application/controllers/acc/Penerimaan_terikat.php
+++ application/controllers/acc/Penerimaan_terikat.php
@@ -17,25 +17,25 @@ class Penerimaan_terikat extends CI_Controller
   function infaq_kotak_jumat()
   {
     $data['kd_akun'] = $this->input->get('kd_akun');
-    $this->load->view('acc/form_input', $data);
+    $this->load->view('acc/form_input_penerimaan_terikat_v', $data);
   }
 
   function infaq_phbi()
   {
     $data['kd_akun'] = $this->input->get('kd_akun');
-    $this->load->view('acc/form_input', $data);
+    $this->load->view('acc/form_input_penerimaan_terikat_v', $data);
   }
 
   function infaq_pengajian()
   {
     $data['kd_akun'] = $this->input->get('kd_akun');
-    $this->load->view('acc/form_input', $data);
+    $this->load->view('acc/form_input_penerimaan_terikat_v', $data);
   }
 
   function infaq_ramadhan()
   {
     $data['kd_akun'] = $this->input->get('kd_akun');
-    $this->load->view('acc/form_input', $data);
+    $this->load->view('acc/form_input_penerimaan_terikat_v', $data);
   }
 }
 
