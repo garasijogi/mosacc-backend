4bdb3312b19d6042601f687e2e07ff5ae2838ecd
Rizuki Ryumada <rizki3366@gmail.com>
2019-11-08 09:14:58 +0700

    Dashboard Progress 2

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -27,6 +27,8 @@ class Homepage extends CI_Controller
     $this->db->trans_start();
     $this->db->query("CREATE DATABASE IF NOT EXISTS $db_dynamic;");
     $this->db->query("USE $db_dynamic;");
+    $this->db->query("CREATE TABLE IF NOT EXISTS `sum_kd_akun` (`kd_sum` varchar(7) NOT NULL, `kd_akun` varchar(5) DEFAULT NULL, `bulan` varchar(2) DEFAULT NULL, `jumlah` double DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+    $this->db->query("CREATE TABLE IF NOT EXISTS `sum_table` (`kd_sum` int(11) NOT NULL, `tabel` varchar(128) NOT NULL, `bulan` varchar(2) DEFAULT NULL, `jumlah` double DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
     $this->db->query("CREATE TABLE IF NOT EXISTS `tr11_penerimaan_tidak_terikat_pending` (`idtr` varchar(16) NOT NULL, `kd_akun` varchar(5) NOT NULL, `tanggal` date NOT NULL, `nominal` double NOT NULL, `nama_pemberi` varchar(256) NOT NULL, `keterangan` text NOT NULL, PRIMARY KEY (`idtr`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
     $this->db->query("CREATE TABLE IF NOT EXISTS `tr12_penerimaan_terikat_pending` (`idtr` varchar(16) NOT NULL, `kd_akun` varchar(5) NOT NULL, `tanggal` date NOT NULL, `nominal` double NOT NULL, `nama_pemberi` varchar(256) NOT NULL, `keterangan` text NOT NULL, PRIMARY KEY (`idtr`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
     $this->db->query("CREATE TABLE IF NOT EXISTS `tr21_pembelian_pending` (`idtr` varchar(16) NOT NULL, `kd_akun` varchar(5) NOT NULL, `tanggal` date NOT NULL, `jenis` varchar(32) NOT NULL, `merek` varchar(112) NOT NULL, `nomor_seri` varchar(112) NOT NULL, `jumlah` int(11) NOT NULL, `keterangan` text NOT NULL, `harga_satuan` int(11) NOT NULL, `total_harga` int(11) NOT NULL, PRIMARY KEY (`idtr`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
